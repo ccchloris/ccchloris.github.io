cfeafc31fa152136909a631102c515c5af87022d
lmk123 <me@limingkai.cn>
2016-06-22 23:37:28 +0800

    调整详情页

--- src/pages/front/detail.vue
+++ src/pages/front/detail.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="wrap" id="article-detail">
-    <div v-if="article">
+    <div v-if="article" class="article">
       <h1 v-text="article.title"></h1>
-      <div>
-        创建时间: <span v-text="article.createTime | toDate"></span><br>
-        最后修改时间: <span v-text="article.lastModifyTime | toDate"></span>
-      </div>
+      <!--<div>-->
+        <!--创建时间: <span v-text="article.createTime | toDate"></span><br>-->
+        <!--最后修改时间: <span v-text="article.lastModifyTime | toDate"></span>-->
+      <!--</div>-->
       <div class="content" v-html="article.content"></div>
 
       <!-- 片段一 点击购买 -->
       <!--<div class="buy-it">-->
-        <!--<div class="info">-->
-          <!--<div class="value">￥63.00</div>-->
-          <!--<div class="likes">2345人喜欢</div>-->
-        <!--</div>-->
-        <!--<div>-->
-          <!--<a target="_blank" href="#">查看详情</a>-->
-        <!--</div>-->
+      <!--<div class="info">-->
+      <!--<div class="value">￥63.00</div>-->
+      <!--<div class="likes">2345人喜欢</div>-->
+      <!--</div>-->
+      <!--<div>-->
+      <!--<a target="_blank" href="#">查看详情</a>-->
+      <!--</div>-->
       <!--</div>-->
     </div>
   </div>
@@ -36,53 +36,64 @@
 
 <style lang="sass" rel="stylesheet/scss">
   #article-detail {
-    .content {
-      margin-top: 20px;
-      margin-bottom: 50px;
-    }
 
-    .buy-it {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 20px;
-      position: relative;
+    .article {
+      width: 600px;
+      margin: 0 auto;
 
-      &:before {
-        content: '';
-        display: block;
-        background: #d8d8d8;
-        width: 2px;
-        height: 88%;
-        position: absolute;
-        top: 5%;
+      .content {
+        margin-top: 20px;
+        margin-bottom: 50px;
+
+        img {
+          width: 100%;
+          height: auto !important;
+        }
       }
 
-      .info {
-        padding-left: 10px;
-      }
+      .buy-it {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+        position: relative;
 
-      .value {
-        color: #f84e4e;
-      }
+        &:before {
+          content: '';
+          display: block;
+          background: #d8d8d8;
+          width: 2px;
+          height: 88%;
+          position: absolute;
+          top: 5%;
+        }
 
-      .likes {
-        color: #999;
-      }
+        .info {
+          padding-left: 10px;
+        }
 
-      a {
-        display: inline-block;
-        width: 100px;
-        text-align: center;
-        font-size: 16px;
-        border: solid thin #ff897d;
-        height: 32px;
-        line-height: 30px;
-        border-radius: 2px;
-        color: #f7554d;
+        .value {
+          color: #f84e4e;
+        }
 
-        &:hover {
-          text-decoration: none;
+        .likes {
+          color: #999;
+        }
+
+        a {
+          display: inline-block;
+          width: 100px;
+          text-align: center;
+          font-size: 16px;
+          border: solid thin #ff897d;
+          height: 32px;
+          line-height: 30px;
+          border-radius: 2px;
+          color: #f7554d;
+
+          &:hover {
+            text-decoration: none;
+          }
         }
       }
     }
