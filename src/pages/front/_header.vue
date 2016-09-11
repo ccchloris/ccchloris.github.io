b1e3f22cfb769c2343d1c5b4216ea237bc21f471
lmk123 <me@limingkai.cn>
2016-09-12 00:22:25 +0800

    导航栏动效

--- src/pages/front/_header.vue
+++ src/pages/front/_header.vue
@@ -5,11 +5,29 @@
   <nav id="nav">
     <div class="header-nav" v-fixed-top>
       <div class="wrap">
-        <ul class="list-unstyled">
+        <ul class="list-unstyled menus-box">
           <li><a href="#">主页</a></li>
-          <li><a href="#">发现美</a></li>
-          <li><a href="#">联系我</a></li>
-          <li><a href="#">售卖甜品</a></li>
+          <li>
+            <a href="#">寻找生活</a>
+            <div class="sub-menus">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">视频</a>
+                </li>
+                <li>
+                  <a href="#">夏日</a>
+                </li>
+                <li>
+                  <a href="#">穿衣</a>
+                </li>
+                <li>
+                  <a href="#">美食</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li><a href="#">文字生活</a></li>
+          <li><a href="#">美丽生活</a></li>
         </ul>
       </div>
     </div>
@@ -32,21 +50,89 @@
       width: 100%;
       z-index: 9999;
 
-      ul {
+      .menus-box {
         margin: 0;
         display: flex;
         justify-content: space-between;
         align-items: center;
         height: $height;
 
-        li {
+        > li {
           flex-grow: 1;
           text-align: center;
-          $height: 60px;
+          position: relative;
+          height: $height;
+          line-height: $height;
+
+          &::after,
+          &::before {
+            $lineHeight: 2px;
+            display: block;
+            position: absolute;
+            top: ($height - $lineHeight) / 2;
+            content: '';
+            opacity: 0;
+            width: 20px;
+            height: $lineHeight;
+            background-color: #fff;
+            transition: all 0.2s;
+          }
+
+          $m: 65px;
+          &:after {
+            right: $m;
+          }
+
+          &:before {
+            left: $m;
+          }
 
           a {
             color: #fff;
             font-size: 18px;
+
+            &:hover {
+              text-decoration: none;
+            }
+          }
+
+          $liHeight: 44px;
+          .sub-menus {
+            position: absolute;
+            top: $height;
+            left: 25%;
+            width: 50%;
+
+            ul {
+              height: 0;
+              overflow: hidden;
+              transition: all 0.2s;
+
+              li {
+                height: $liHeight;
+                line-height: $liHeight;
+                background-color: #000;
+                color: #fff;
+              }
+            }
+          }
+
+          &:hover {
+            .sub-menus ul {
+              height: $liHeight * 4;
+            }
+
+            &:after,
+            &:before {
+              opacity: 1;
+            }
+
+            &:before {
+              left: 50px;
+            }
+            &:after {
+              right: 50px;
+            }
           }
         }
       }
